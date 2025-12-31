eb8cfbdb0efa3c6de23a761db09b15760b2c50c8
Yuji Oya <maxrahmen@gmail.com>
2025-12-31 09:58:33 +0900

    feat: @vercel/functionsを追加し、DynamoDBクライアントの認証を設定

--- src/dynamoClient.ts
+++ src/dynamoClient.ts
@@ -1,5 +1,11 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { awsCredentialsProvider } from "@vercel/functions/oidc";
+
+const credentials = awsCredentialsProvider({
+  roleArn: process.env.AWS_ROLE_ARN ?? "", // Vercelの環境変数に設定したロールARN
+});
 
 export const dynamo = new DynamoDBClient({
   region: process.env.AWS_REGION!,
+  credentials: process.env.VERCEL ? credentials : undefined,
 });
