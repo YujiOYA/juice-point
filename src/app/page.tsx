9af66a7084bf3630ca6424aa5c8bf644d7bd6640
Yuji Oya <maxrahmen@gmail.com>
2026-03-20 11:54:22 +0900

    feat: ページコンポーネントに動的レンダリングを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import PageClient from "@/app/PageClient";
 import { getUsers, getTasks, getSubmissions } from "@/lib/dynamoDbApi";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const [users, tasks, submissions] = await Promise.all([
     getUsers(),
